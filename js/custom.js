0e0949a3a995244a39cad3d72f65b90efd403bc5
Muhamed <muhamed.subasic@stu.ibu.edu.ba>
2025-03-23 22:55:24 +0100

    Spapp done, additional folders prepared and adjustments

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  
+  var app = $.spapp({
+    defaultView: "#home",
+    templateDir: "./tpl/",
+    pageNotFound: "error_404",
+  });
+
+  // Page Routes
+  app.route({ view: "home" });
+  app.route({ view: "about" });
+  app.route({ view: "service" });
+  app.route({ view: "feature" });
+  app.route({ view: "cars" });
+  app.route({ view: "team" });
+  app.route({ view: "testimonial" });
+  app.route({ view: "contact" });
+
+  // Highlight the active link in navbar
+  function highlightActiveLink() {
+    $(".navbar-nav .nav-link").removeClass("active");
+    var currentHash = window.location.hash;
+    $('.navbar-nav .nav-link[href="' + currentHash + '"]').addClass("active");
+  }
+  
+  app.run();
+
+  highlightActiveLink();
+  $(window).on("hashchange", function () {
+    highlightActiveLink();
+  });
+});
